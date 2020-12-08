079bda8d1834acb9d9d48077838d9036f43f2177
Maxim Zebzeev <maksimzeb@yandex.ru>
2020-12-08 14:27:44 +0500

    asda

--- js/binary searh.js
+++ js/binary searh.js
@@ -12,26 +12,43 @@ let result = NumberSto.slice(lengthStoMinus, 390);
 let lengthStoPlus = NumberSto.length;
 
 // Проверка
-console.log(result);
+console.log(NumberSto);
+
 // переменные для плюса
-let stoone = 101;
-let NumberFitftiPlus = 50; // первое число с которого начинается отсчет в лево или вправо в зависимости от ответа пользователя
-let FirstPos = result.indexOf(NumberFitftiPlus); // возращает индекс первого значения
-let EndPos = result.lastIndexOf(stoone); //
+let OneHudred = 101;
+let Fifty = 50; // первое число с которого начинается отсчет в лево или вправо в зависимости от ответа пользователя
+let StartPos = result.indexOf(Fifty); // возращает индекс первого значения
+let EndPos = result.lastIndexOf(OneHudred); //
 
-// переменные для минуса
-let zerto = 0;
-let NumberFitftiMinus = 51;
-let FirstPosMinus = result.indexOf(zerto); // возращает индекс первого значения
-let EndPosMinus = result.lastIndexOf(NumberFitftiMinus); //
-// Проверка
-// console.log(EndPos);
-
-function chet () {
-	NumberSto = NumberSto.slice(FirstPos,EndPos);
+function noname () {
+	return 
 }
- function uslovie () {
- 	var FirstAnswer = prompt("Это число равно,больше или меньше " + NumberFitftiPlus + " ?");
+// Функция которая делит строку в зависимости от ответа пользователя
+function chet (StartPos,EndPos) {
+	NumberSto = NumberSto.slice(StartPos,EndPos);
+}
+
+ function conditional () {
+ 	var FirstAnswer = prompt("Это число равно, больше или меньше " + Fifty + " ?");
+ 	var Result = FirstAnswer.toLowerCase();
+ 	switch (Result) {
+ 		case "больше":
+ 			chet (StartPos,EndPos);
+ 			console.log(NumberSto);
+ 		break;
+ 		case "меньше":
+ 			Fifty -= 50;
+ 			OneHudred -= 50;
+ 			chet (StartPos,EndPos);
+ 			console.log(Fifty);
+ 		break;
+ 		case "равно":
+ 			alert("Ура! Я угадал. А Вы смелый человек раз решились со машиной играть")
+ 		break;
+ 		case null:
+			uslovie ();	
+ 		break;
+ 	}
  }
 
 
@@ -56,7 +73,13 @@ function chet () {
 
 
 
-
+// переменные для минуса
+// let zerto = 0;
+// let NumberFitftiMinus = 51;
+// let FirstPosMinus = result.indexOf(zerto); // возращает индекс первого значения
+// let EndPosMinus = result.lastIndexOf(NumberFitftiMinus); //
+// Проверка
+// console.log(EndPos);
 
 
 
