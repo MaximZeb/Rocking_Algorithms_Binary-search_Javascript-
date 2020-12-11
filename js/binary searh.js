b3ecd87b4446e5662966f67906f786220d2bd19d
Maxim Zebzeev <maksimzeb@yandex.ru>
2020-12-11 13:37:49 +0500

    Added edited code

--- js/binary searh.js
+++ js/binary searh.js
@@ -1,11 +1,11 @@
 // Блок кода который прописывает 1 до 100 число с типом данных string
-let NumberSto = "";
+let NumberSto = ""; // здесь лежит строка от 1 до 100 после цикла 
 let x = 1;
 let poru = " ";
 for (x = 1; x < 101; x++) {
 	NumberSto += x + poru;  // "1, " + 2 + ", " + 3 ", " ...
 }
-let result = NumberSto.trim();
+let result = NumberSto.trim(); // перерменая которая будет перезаписыватьна протяжение все программмы как и неиже отсальные переменнве
 // Убирает пробел после 100
 function space () {
 	return result = result.trim();
@@ -13,19 +13,18 @@ function space () {
 }
 
 
-// Проверка
+// Проверка без пробелов
 console.log(result);
 
 // переменные
-let kostM = 6;
-let kost = 8;
-let Fifty = 50;
+let kostM = 6; // переменая разница для плюс
+let kost = 8; // переменая разница для минус
+let Fifty = 50; // переменая которая будет меняться
 let StartPos; // возращает индекс первого значения
 let EndPos; //возращает индекс последнего значения
 let Numberstr = 50; // число для окна
-console.log();
 
-// Функция которая делит строку в зависимости от ответа пользователя
+// Функция которая делит строку в зависимости от ответа пользователя минус
 function chetMinus () {
 	StartPos = 0;
 	EndPos = result.length;
@@ -42,6 +41,7 @@ function chetMinus () {
 	result = result.slice(StartPos,EndPos);
 }
 
+// Функция которая делит строку в зависимости от ответа пользователя плюс
 function chet () {
 	StartPos = result.length;
 	function proverka (){
@@ -58,7 +58,7 @@ function chet () {
 
 	result = result.slice(StartPos,EndPos);
 }
-
+// Первый вопрос после нажатия кнопки "Хорошо, давай!"
  function conditional () {
  	var FirstAnswer = prompt("Это число равно, больше или меньше " + Numberstr + " ?");
  	var Result = FirstAnswer.toLowerCase();
@@ -67,6 +67,7 @@ function chet () {
  			Numberstr += 25;
  			space ();
  			chet ();
+ 			space ()
  			console.log(StartPos);
  			console.log(EndPos);
  			console.log(result);
@@ -77,6 +78,7 @@ function chet () {
  			Numberstr -= 25;
  			space ();
  			chetMinus ();
+ 			space ();
  			console.log(StartPos);
  			console.log(EndPos);
  			console.log(result);
@@ -99,8 +101,8 @@ function chet () {
  	switch (Result) {
  		case "больше":
  			Numberstr += 13;
- 			space ();
  			chet ();
+ 			space ();
  			console.log(StartPos);
  			console.log(EndPos);
  			console.log(result);
@@ -109,8 +111,8 @@ function chet () {
  		break;
  		case "меньше":
  			Numberstr -= 13;
- 			space ();
  			chetMinus ();
+ 			space ();
  			console.log(StartPos);
  			console.log(EndPos);
  			console.log(result);
@@ -121,7 +123,7 @@ function chet () {
  			alert("Ура! Я угадал. А Вы смелый человек раз решились с машиной играть.")
  		break;
  		case null:
-			conditional ();	
+			conditional2 ();	
  		break;
  	}
  }
@@ -154,7 +156,7 @@ function conditional3 () {
  			alert("Ура! Я угадал. А Вы смелый человек раз решились с машиной играть.")
  		break;
  		case null:
-			conditional ();	
+			conditional3 ();	
  		break;
  	}
  }
@@ -186,7 +188,7 @@ function conditional4 () {
  			alert("Ура! Я угадал. А Вы смелый человек раз решились с машиной играть.")
  		break;
  		case null:
-			conditional ();	
+			conditional4 ();	
  		break;
  	}
  }
@@ -197,8 +199,8 @@ function conditional5 () {
  	switch (Result) {
  		case "больше":
  			Numberstr += 2;
- 			space ();
  			chet ();
+ 			space ();
  			console.log(StartPos);
  			console.log(EndPos);
  			console.log(result);
@@ -206,8 +208,8 @@ function conditional5 () {
  		break;
  		case "меньше":
  			Numberstr -= 2;
- 			space ();
  			chetMinus ();
+ 			space ();
  			console.log(StartPos);
  			console.log(EndPos);
  			console.log(result);
@@ -217,7 +219,7 @@ function conditional5 () {
  			alert("Ура! Я угадал. А Вы смелый человек раз решились с машиной играть.")
  		break;
  		case null:
-			conditional ();	
+			conditional5 ();	
  		break;
  	}
  }
@@ -228,8 +230,8 @@ function conditional6 () {
  	switch (Result) {
  		case "больше":
  			Numberstr += 1;
- 			space ();
  			chet ();
+ 			space ();
  			console.log(StartPos);
  			console.log(EndPos);
  			console.log(result);
@@ -237,8 +239,8 @@ function conditional6 () {
  		break;
  		case "меньше":
  			Numberstr -= 1;
- 			space ();
  			chetMinus ();
+ 			space ();
  			console.log(StartPos);
  			console.log(EndPos);
  			console.log(result);
@@ -248,7 +250,7 @@ function conditional6 () {
  			alert("Ура! Я угадал. А Вы смелый человек раз решились с машиной играть.")
  		break;
  		case null:
-			conditional ();	
+			conditional6 ();	
  		break;
  	}
  }
@@ -260,6 +262,7 @@ function conditional7 () {
  		case "больше":
  			Numberstr += 25;
  			chet ();
+ 			space ();
  			console.log(StartPos);
  			console.log(EndPos);
  			console.log(result);
@@ -267,6 +270,7 @@ function conditional7 () {
  		case "меньше":
  			Numberstr -= 25;
  			chetMinus ();
+ 			space ();
  			console.log(StartPos);
  			console.log(EndPos);
  			console.log(result);
@@ -275,14 +279,14 @@ function conditional7 () {
  			alert("Ура! Я угадал. А Вы смелый человек раз решились с машиной играть.")
  		break;
  		case null:
-			conditional ();	
+			conditional7 ();	
  		break;
  	}
  }
 
 
 
-// Полифилы трем 
+// Полифилы трим 
 if (!String.prototype.trim) {
   (function() {
     // Вырезаем BOM и неразрывный пробел
@@ -291,70 +295,3 @@ if (!String.prototype.trim) {
     };
   })();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// переменные для минуса
-// let zerto = 0;
-// let NumberFitftiMinus = 51;
-// let FirstPosMinus = result.indexOf(zerto); // возращает индекс первого значения
-// let EndPosMinus = result.lastIndexOf(NumberFitftiMinus); //
-// Проверка
-// console.log(EndPos);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Функция с первым вопросом
-// function firstQuets () {
-// 	var FirstAnswer = prompt("Это число равно,больше или меньше 50?");
-// 	var Result = FirstAnswer.toLowerCase();
-// 	if (Result === "меньше") {
-// 		NumberSto = NumberSto.slice(FirstPos,EndPos);
-// 		NumberFitftiPlus += 25;
-// 		console.log(NumberSto);
-// 		Twoquest (true);
-// 	} else if (Result === "больше") {
-// 		NumberFitftiMinus -= 25;
-// 		NumberSto = NumberSto.slice(FirstPosMinus,EndPosMinus);
-// 		console.log(NumberSto);
-// 		Twoquest ();
-// 	} else if (Result === "равно") {
-// 		alert("Ура! А Вы смелый человек раз решились со машиной играть")
-// 	} else {
-// 		prompt("Напиши пожалйуста Да или Нет. Это число равно 50 или больше 50 или меньше?")
-// 	}
-// }
-
-// function Twoquest (answer) {
-// 	if(answer === true){
-// 		var TwoAnswer = prompt("Это число равно,больше или меньше 50?");
-// 	}
-	
-// } 
